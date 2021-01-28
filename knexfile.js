75b0d53411da189f5d53798ca227e2c83eeb012d
Samuel Lee <samuelxlee@gmail.com>
2021-01-28 14:21:08 -0800

    installed additional dependencies, set up project architecture and knex file for database access

---
+++ knexfile.js
@@ -0,0 +1,29 @@
+module.exports = {
+
+  development: {
+    client: 'sqlite3',
+    useNullAsDefault: true,
+    migrations: { directory: './data/migrations' },
+    seeds: { directory: './data/seeds' },
+    connection: {
+      filename: './dev.sqlite3'
+    }
+  },
+
+  testing: {
+    client: 'sqlite3',
+    useNullAsDefault: true,
+    migrations: { directory: './data/migrations' },
+    seeds: { directory: './data/seeds' },
+    connection: {
+      filename: './data/test.db3',
+    },
+  },
+
+  staging: {
+  },
+
+  production: {
+  }
+
+};
